ec182a1b6669ae2fb8f1bbbe5b2b27388a8f9daa
Prashant Srivastava <srprash@amazon.com>
2020-06-05 03:14:10 +0530

    Refactored to more modular code. Added 1 second timeout and a retry attempt

--- lib/aws-xray-sdk/plugins/ec2.rb
+++ lib/aws-xray-sdk/plugins/ec2.rb
@@ -4,7 +4,7 @@ require 'aws-xray-sdk/logger'
 
 module XRay
   module Plugins
-    # A plugin that gets the EC2 instance-id and AZ if running on an EC2 instance.
+    # A plugin that gets the EC2 instance_id, availabiity_zone, instance_type, and ami_id if running on an EC2 instance.
     module EC2
       include Logging
 
@@ -27,17 +27,11 @@ module XRay
 
       def self.get_token
         token_uri = URI(METADATA_BASE_URL + '/api/token')
+
+        req = Net::HTTP::Put.new(token_uri)
+        req['X-aws-ec2-metadata-token-ttl-seconds'] = '60'
         begin
-          req = Net::HTTP::Put.new(token_uri)
-          req['X-aws-ec2-metadata-token-ttl-seconds'] = '60'
-          res = Net::HTTP.start(token_uri.hostname) { |http|
-            http.request(req)
-          }
-          if res.code == '200'
-            res.body
-          else
-            raise(StandardError.new("Unsuccessful response:: " + res.code))
-          end
+          return do_request(req)
         rescue StandardError => e
           Logging.logger.warn %(can not get the IMDSv2 token due to: #{e.message}.)
           ''
@@ -46,19 +40,15 @@ module XRay
 
       def self.get_metadata(token)
         metadata_uri = URI(METADATA_BASE_URL + '/dynamic/instance-identity/document')
+
+        req = Net::HTTP::Get.new(metadata_uri)
+        if token != ''
+          req['X-aws-ec2-metadata-token'] = token
+        end
+
         begin
-          req = Net::HTTP::Get.new(metadata_uri)
-          if token != ''
-            req['X-aws-ec2-metadata-token'] = token
-          end
-          res = Net::HTTP.start(metadata_uri.hostname) { |http|
-            http.request(req)
-          }
-          if res.code == '200'
-            return parse_metadata(res.body)
-          else
-            raise(StandardError.new("Unsuccessful response:: " + res.code))
-          end
+          metadata_json = do_request(req)
+          return parse_metadata(metadata_json)
         rescue StandardError => e
           Logging.logger.warn %(can not get the ec2 instance metadata due to: #{e.message}.)
           {}
@@ -74,6 +64,30 @@ module XRay
         metadata['ami_id'] = data['imageId']
 
         metadata
+      end
+
+      def self.do_request(request)
+        begin
+          response = Net::HTTP.start(request.uri.hostname, read_timeout: 1) { |http|
+            http.request(request)
+          }
+
+          if response.code == '200'
+            return response.body
+          else
+            raise(StandardError.new('Unsuccessful response::' + response.code + '::' + response.message))
+          end
+        rescue StandardError => e
+          # Two attempts in total to complete the request successfully
+          @retries ||= 0
+          if @retries < 1
+            @retries += 1
+            retry
+          else
+            Logging.logger.warn %(Failed to complete request due to: #{e.message}.)
+            raise e
+          end
+        end
       end
     end
   end
