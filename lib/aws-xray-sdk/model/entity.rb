90dae7508ead01e8feec193f0a7f32f6e4e41193
Chabert Etienne <etienne.chabert@gmail.com>
2023-03-03 19:12:32 +0100

    Prevent warnings variable @metadata not initialised (#86)
    * Use metadata method instead of @metadata variable
    
    * empty
    
    * Go back to @metadata, but with metadata before to ensure initialization
    
    * Update entity.rb
    
    * Update entity.rb
    
    ---------
    
    Co-authored-by: Etienne Chabert <etienne.chabert@solarisbank.de>

--- lib/aws-xray-sdk/model/entity.rb
+++ lib/aws-xray-sdk/model/entity.rb
@@ -155,7 +155,8 @@ module XRay
       if (a = annotations.to_h) && !a.empty?
         h[:annotations] = a
       end
-      if (m = @metadata) && !m.to_h.empty?
+      # make sure @metadata is defined before evaluating it, to prevent warning `variable @metadata not initialized`
+      if (defined?(@metadata) && m = @metadata) && !m.to_h.empty?
         h[:metadata] = m.to_h
       end
 
