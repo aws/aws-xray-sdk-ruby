74037ec5b049edaa849f8825328ffc9842866d71
Etienne Chabert <etienne.chabert@solarisbank.de>
2023-09-11 10:53:22 +0200

    Copy paste all EC2 tests as starting point for ECS

--- test/aws-xray-sdk/tc_plugin.rb
+++ test/aws-xray-sdk/tc_plugin.rb
@@ -20,6 +20,10 @@ class TestPlugins < Minitest::Test
       .to_raise(StandardError)
     stub_request(:any, XRay::Plugins::EC2::METADATA_BASE_URL + '/dynamic/instance-identity/document')
       .to_raise(StandardError)
+    stub_request(:any, XRay::Plugins::ECS::METADATA_BASE_URL + '/api/token')
+      .to_raise(StandardError)
+    stub_request(:any, XRay::Plugins::ECS::METADATA_BASE_URL + '/dynamic/instance-identity/document')
+      .to_raise(StandardError)
     XRay::Plugins::EC2.aws
     XRay::Plugins::ECS.aws
     WebMock.reset!
@@ -78,6 +82,77 @@ class TestPlugins < Minitest::Test
 
     expected = {}
     assert expected, XRay::Plugins::EC2.aws
+    WebMock.reset!
+  end
+
+  def test_get_runtime_context
+    XRay::Plugins::ElasticBeanstalk.aws
+    stub_request(:any, XRay::Plugins::EC2::METADATA_BASE_URL + '/api/token')
+      .to_raise(StandardError)
+    stub_request(:any, XRay::Plugins::EC2::METADATA_BASE_URL + '/dynamic/instance-identity/document')
+      .to_raise(StandardError)
+    stub_request(:any, XRay::Plugins::ECS::METADATA_BASE_URL + '/api/token')
+      .to_raise(StandardError)
+    stub_request(:any, XRay::Plugins::ECS::METADATA_BASE_URL + '/dynamic/instance-identity/document')
+      .to_raise(StandardError)
+    XRay::Plugins::EC2.aws
+    XRay::Plugins::ECS.aws
+    WebMock.reset!
+  end
+
+  def test_ecs_metadata_v2_successful
+    dummy_json = '{\"availabilityZone\" : \"us-east-2a\", \"imageId\" : \"ami-03cca83dd001d4666\",
+                  \"instanceId\" : \"i-07a181803de94c666\", \"instanceType\" : \"t3.xlarge\"}'
+
+    stub_request(:put, XRay::Plugins::ECS::METADATA_BASE_URL + '/api/token')
+      .to_return(status: 200, body: 'some_token', headers: {})
+
+    stub_request(:get, XRay::Plugins::ECS::METADATA_BASE_URL + '/dynamic/instance-identity/document')
+      .to_return(status: 200, body: dummy_json, headers: {})
+
+    expected = {
+      ec2: {
+        instance_id: 'i-07a181803de94c666',
+        availability_zone: 'us-east-2a',
+        instance_type: 't3.xlarge',
+        ami_id: 'ami-03cca83dd001d4666'
+      }
+    }
+    assert expected, XRay::Plugins::ECS.aws
+    WebMock.reset!
+  end
+
+  def test_ecs_metadata_v1_successful
+    dummy_json = '{\"availabilityZone\" : \"cn-north-1a\", \"imageId\" : \"ami-03cca83dd001d4111\",
+                  \"instanceId\" : \"i-07a181803de94c111\", \"instanceType\" : \"t2.xlarge\"}'
+
+    stub_request(:put, XRay::Plugins::ECS::METADATA_BASE_URL + '/api/token')
+      .to_raise(StandardError)
+
+    stub_request(:get, XRay::Plugins::ECS::METADATA_BASE_URL + '/dynamic/instance-identity/document')
+      .to_return(status: 200, body: dummy_json, headers: {})
+
+    expected = {
+      ec2: {
+        instance_id: 'i-07a181803de94c111',
+        availability_zone: 'cn-north-1a',
+        instance_type: 't2.xlarge',
+        ami_id: 'ami-03cca83dd001d4111'
+      }
+    }
+    assert expected, XRay::Plugins::ECS.aws
+    WebMock.reset!
+  end
+  
+  def test_ecs_metadata_fail
+    stub_request(:put, XRay::Plugins::ECS::METADATA_BASE_URL + '/api/token')
+      .to_raise(StandardError)
+
+    stub_request(:get, XRay::Plugins::ECS::METADATA_BASE_URL + '/dynamic/instance-identity/document')
+      .to_raise(StandardError)
+
+    expected = {}
+    assert expected, XRay::Plugins::ECS.aws
     WebMock.reset!
   end
 end
