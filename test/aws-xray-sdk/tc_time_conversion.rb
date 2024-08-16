cf11ea7dcca190360153338c53dcc1106bce2696
ibnjunaid <osamabinjunaid36@gmail.com>
2024-08-17 05:05:58 +0530

    Fix unit tests

--- test/aws-xray-sdk/tc_time_conversion.rb
+++ test/aws-xray-sdk/tc_time_conversion.rb
@@ -3,7 +3,7 @@ require 'aws-xray-sdk/utils/math_utils'
 class TestGetTimeInSeconds < Minitest::Test
     def convert_time_from_milliseconds
         time_in_seconds = convert_time_in_seconds 1.723627099460531E12
-        assert_equal time_in_seconds 172362709.9460531
+        assert_equal time_in_seconds 1723627099.460531
     end
 
     def convert_time_from_seconds
